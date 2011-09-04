6a38ac490e13c45585480bdd7146234105fd4bfd
Aneesh P U <aneeshpu@gmail.com>
2011-09-04 13:18:55 +0530

    Refactored into multiple test classes

--- src/com/vanhelsing/it/ClassificationTest.java
+++ src/com/vanhelsing/it/ClassificationTest.java
@@ -7,49 +7,31 @@ import android.test.ProviderTestCase2;
 import android.util.Log;
 
 import com.vanhelsing.contentProvider.ClassificationTable;
-import com.vanhelsing.contentProvider.FeatureTable;
 import com.vanhelsing.contentProvider.SpamContentProvider;
 
-public class SpamContentProviderTest extends ProviderTestCase2 {
+public class ClassificationTest extends ProviderTestCase2<SpamContentProvider> {
 
-	public SpamContentProviderTest() {
-		super(SpamContentProvider.class, "com.vanhelsing.contentProvider");
+	public ClassificationTest() {
+		super(SpamContentProvider.class, SpamContentProvider.AUTHORITY);
 	}
-	
+
 	@Override
 	public void testAndroidTestCaseSetupProperly() {
 		super.testAndroidTestCaseSetupProperly();
 	}
-	
-	public void testInsertFeature() throws Exception {
-		ContentValues contentValues = spamData();
-		Uri insertedFeature = getProvider().insert(FeatureTable.URI, contentValues);
-		Log.i("vanhelsing" , insertedFeature.toString());
-		
-		Cursor cursor = getProvider().query(FeatureTable.URI, new String[] { FeatureTable.DB_COL_NAME }, null, null, null);
-		cursor.moveToFirst();
-		String feature = cursor.getString(cursor.getColumnIndex(FeatureTable.DB_COL_NAME));
-		assertEquals("money", feature);
-	}
 
-	private ContentValues spamData() {
-		ContentValues contentValues = new ContentValues();
-		contentValues.put(FeatureTable.DB_COL_NAME, "money");
-		return contentValues;
-	}
-	
 	public void testInsertClassification() throws Exception {
 		ContentValues contentValues = classificationData();
 		Uri insertedFeature = getProvider().insert(ClassificationTable.URI, contentValues);
-		Log.i("vanhelsing" , insertedFeature.toString());
-		
+		Log.i("vanhelsing", insertedFeature.toString());
+
 		Cursor cursor = getProvider().query(ClassificationTable.URI, new String[] { ClassificationTable.DB_COL_NAME, ClassificationTable.DB_COL_DOCUMENT_COUNT }, null, null, null);
 		cursor.moveToFirst();
 		String classification = cursor.getString(cursor.getColumnIndex(ClassificationTable.DB_COL_NAME));
 		int document_count = cursor.getInt(cursor.getColumnIndex(ClassificationTable.DB_COL_DOCUMENT_COUNT));
 		assertEquals("good", classification);
 		assertEquals(1, document_count);
-		
+
 	}
 
 	private ContentValues classificationData() {
---
+++ src/com/vanhelsing/it/FeatureTest.java
@@ -0,0 +1,42 @@
+package com.vanhelsing.it;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.test.ProviderTestCase2;
+import android.util.Log;
+
+import com.vanhelsing.contentProvider.ClassificationTable;
+import com.vanhelsing.contentProvider.FeatureTable;
+import com.vanhelsing.contentProvider.SpamContentProvider;
+
+public class FeatureTest extends ProviderTestCase2 {
+
+	public FeatureTest() {
+		super(SpamContentProvider.class, "com.vanhelsing.contentProvider");
+	}
+	
+	@Override
+	public void testAndroidTestCaseSetupProperly() {
+		super.testAndroidTestCaseSetupProperly();
+	}
+	
+	public void testInsertFeature() throws Exception {
+		ContentValues contentValues = features();
+		Uri insertedFeature = getProvider().insert(FeatureTable.URI, contentValues);
+		Log.i("vanhelsing" , insertedFeature.toString());
+		
+		Cursor cursor = getProvider().query(FeatureTable.URI, new String[] { FeatureTable.DB_COL_NAME }, null, null, null);
+		cursor.moveToFirst();
+		String feature = cursor.getString(cursor.getColumnIndex(FeatureTable.DB_COL_NAME));
+		assertEquals("money", feature);
+	}
+
+	private ContentValues features() {
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(FeatureTable.DB_COL_NAME, "money");
+		return contentValues;
+	}
+	
+
+}
