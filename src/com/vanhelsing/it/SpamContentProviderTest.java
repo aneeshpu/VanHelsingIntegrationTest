ea1cd93045f85bcc13886960572470d64be3bb8c
Aneesh P U <aneeshpu@gmail.com>
2011-09-04 11:44:31 +0530

    Can insert Classifications as well

--- src/com/vanhelsing/it/SpamContentProviderTest.java
+++ src/com/vanhelsing/it/SpamContentProviderTest.java
@@ -6,6 +6,8 @@ import android.net.Uri;
 import android.test.ProviderTestCase2;
 import android.util.Log;
 
+import com.vanhelsing.contentProvider.ClassificationTable;
+import com.vanhelsing.contentProvider.FeatureTable;
 import com.vanhelsing.contentProvider.SpamContentProvider;
 
 public class SpamContentProviderTest extends ProviderTestCase2 {
@@ -21,28 +23,40 @@ public class SpamContentProviderTest extends ProviderTestCase2 {
 	
 	public void testInsertFeature() throws Exception {
 		ContentValues contentValues = spamData();
-		Uri insertedFeature = getProvider().insert(SpamContentProvider.FeatureTable.FEATURE_URI, contentValues);
+		Uri insertedFeature = getProvider().insert(FeatureTable.URI, contentValues);
 		Log.i("vanhelsing" , insertedFeature.toString());
 		
-		Cursor cursor = getProvider().query(SpamContentProvider.FeatureTable.FEATURE_URI, new String[] { SpamContentProvider.FeatureTable.DB_COL_NAME }, null, null, null);
+		Cursor cursor = getProvider().query(FeatureTable.URI, new String[] { FeatureTable.DB_COL_NAME }, null, null, null);
 		cursor.moveToFirst();
-		String feature = cursor.getString(cursor.getColumnIndex(SpamContentProvider.FeatureTable.DB_COL_NAME));
+		String feature = cursor.getString(cursor.getColumnIndex(FeatureTable.DB_COL_NAME));
 		assertEquals("money", feature);
 	}
 
 	private ContentValues spamData() {
 		ContentValues contentValues = new ContentValues();
-		contentValues.put(SpamContentProvider.FeatureTable.DB_COL_NAME, "money");
+		contentValues.put(FeatureTable.DB_COL_NAME, "money");
 		return contentValues;
 	}
 	
 	public void testInsertClassification() throws Exception {
+		ContentValues contentValues = classificationData();
+		Uri insertedFeature = getProvider().insert(ClassificationTable.URI, contentValues);
+		Log.i("vanhelsing" , insertedFeature.toString());
+		
+		Cursor cursor = getProvider().query(ClassificationTable.URI, new String[] { ClassificationTable.DB_COL_NAME, ClassificationTable.DB_COL_DOCUMENT_COUNT }, null, null, null);
+		cursor.moveToFirst();
+		String classification = cursor.getString(cursor.getColumnIndex(ClassificationTable.DB_COL_NAME));
+		int document_count = cursor.getInt(cursor.getColumnIndex(ClassificationTable.DB_COL_DOCUMENT_COUNT));
+		assertEquals("good", classification);
+		assertEquals(1, document_count);
 		
 	}
 
 	private ContentValues classificationData() {
-		// TODO Auto-generated method stub
-		return null;
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(ClassificationTable.DB_COL_NAME, "good");
+		contentValues.put(ClassificationTable.DB_COL_DOCUMENT_COUNT, 1);
+		return contentValues;
 	}
 
 }
